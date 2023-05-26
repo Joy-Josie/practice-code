6d4530184bb37edca4c1342ff7b1beab9fb1f1ff
wangzhuoyi <757983207@qq.com>
2023-05-26 10:56:09 +0800

    wzy

--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
-  // This starter template is using Vue 3 <script setup> SFCs
-  // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-  import HelloWorld from './components/HelloWorld.vue'</script>
+import { ref } from 'vue'
+
+const arr = ref([1, 2, 3])
+</script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div v-for="item in arr">{{ item }}</div>
 </template>
 
-<style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
-</style>
+<style></style>
