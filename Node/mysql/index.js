cfcd62686eb401325ac234c893bd76cb24de616c
WangZhuoYi <13317149311@163.com>
2024-08-12 11:16:26 +0800

    nodejs

--- Node/mysql/index.js
+++ Node/mysql/index.js
@@ -1,33 +1,34 @@
+/*
+ * @Author: WangZhuoYi 13317149311@163.com
+ * @Date: 2023-06-08 17:25:13
+ * @LastEditors: WangZhuoYi 13317149311@163.com
+ * @LastEditTime: 2023-07-04 18:09:25
+ * @FilePath: /practice-code/Node/mysql/index.js
+ * @Description:
+ */
 const mysql = require('mysql')
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
-  port: '3307',
-  database: 'wang'
+	host: 'localhost',
+	user: 'root',
+	password: '123456',
+	database: 'demo',
 })
 
 // 连接数据库
-connection.connect((err) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log('connection success!')
-  }
+connection.connect(err => {
+	if (err) {
+		console.log(err)
+	} else {
+		console.log('connection success!')
+	}
 })
 
 // 执行查询语句
-connection.query('SELECT * FROM student', (err, res, field) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log(res)
-    console.log(field)
-  }
-})
-
-// 删除表
-const sqlStr = 'drop table student'
-connection.query(sqlStr, (err, res, fie) => {
-  console.log(res)
+connection.query('SELECT * FROM users', (err, res, field) => {
+	if (err) {
+		console.log(err)
+	} else {
+		console.log(res)
+		console.log(field)
+	}
 })
